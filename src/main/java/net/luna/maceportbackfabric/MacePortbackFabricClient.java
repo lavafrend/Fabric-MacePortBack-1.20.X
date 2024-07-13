ad601708b041a67913ee1fb6a0eca424cabd4c76
Luna <thephantomclaw588@gmail.com>
2024-07-13 12:10:51 -0600

    i think i did this right

---
+++ src/main/java/net/luna/maceportbackfabric/MacePortbackFabricClient.java
@@ -0,0 +1,10 @@
+package net.luna.maceportbackfabric;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MacePortbackFabricClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
